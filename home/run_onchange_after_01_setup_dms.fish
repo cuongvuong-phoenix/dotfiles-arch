8504644921a3a6bcf9d984c6341ba6a5d8e11e3d
Cuong Phoenix <cuongvuong.phoenix@gmail.com>
2026-03-17 14:34:10 +0700

    feat: `dms` - re-add `calculator` plugin & adjust `emojiLauncher` plugin to include meaningful emojis only
    Otherwise, it would contain a lot of unncessary emojis like Nerd-font
    emojis, unicode characters,... which makes it extremely hard to find
    good emoji via the DMS launcher.

--- home/run_onchange_after_01_setup_dms.fish
+++ home/run_onchange_after_01_setup_dms.fish
@@ -14,4 +14,11 @@ end
 
 install_plugin_if_not_exist dankPomodoroTimer "Dank Pomodoro Timer"
 install_plugin_if_not_exist emojiLauncher "Emoji & Unicode Launcher"
+install_plugin_if_not_exist calculator "Calculator"
 
+# After
+printf "\n🧹 Removing unnecessary emojis from 'Emoji & Unicode Launcher' plugin..."
+rsync -av ~/.local/share/chezmoi/dms-emojiLauncher-data/ ~/.config/DankMaterialShell/plugins/emojiLauncher
+~/.config/DankMaterialShell/plugins/emojiLauncher/scripts/generate_catalog.py
+echo '✅ Done! Restarting DMS...'
+dms restart
