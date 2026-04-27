a3fe282cfa2304b7545be37b117a13351b4ef0a5
Cuong Phoenix <cuongvuong.phoenix@gmail.com>
2026-04-27 15:33:24 +0700

    feat: `niri`, `dms` - apply blur effect
    thanks to new Niri version 26.04

--- home/dot_config/xdg-desktop-portal-termfilechooser/executable_yazi-wrapper.sh
+++ home/dot_config/xdg-desktop-portal-termfilechooser/executable_yazi-wrapper.sh
@@ -29,7 +29,7 @@ else
     set -- --chooser-file="$out" "$path"
 fi
 
-command="kitty --class=file_chooser -T 'Terminal File Chooser' -o background_opacity=1 -e yazi"
+command="kitty --class=file_chooser -T 'Terminal File Chooser' -e yazi"
 for arg in "$@"; do
     # escape double quotes
     escaped=$(printf "%s" "$arg" | sed 's/"/\\"/g')
