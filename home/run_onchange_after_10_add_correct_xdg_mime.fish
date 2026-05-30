8d924b0f2712a7776816feed356bcc01212b87fe
Cuong Phoenix <cuongvuong.phoenix@gmail.com>
2026-05-30 16:04:20 +0700

    feat: use Brave as the default browser

--- home/run_onchange_after_10_add_correct_xdg_mime.fish
+++ home/run_onchange_after_10_add_correct_xdg_mime.fish
@@ -6,8 +6,11 @@ function set_mime_types_to_app -a app
   set -l mime_types (grep "MimeType=" "/usr/share/applications/$app" | sed 's/MimeType=//' | tr ';' '\n')
 
   xdg-mime default $app $mime_types
-  printf "✅ Successfully set following mime types:\n$(string join '\n' $mime_types)\nto '$app'\n\n"
+  printf "✅ Successfully set following mime types to '$app':\n$(string join '\n' $mime_types)\n\n"
 end
+
+# Browser
+set_mime_types_to_app brave-browser.desktop
 
 # Images
 set_mime_types_to_app qimgv.desktop
